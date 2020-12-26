cbf516f674c1531536c7b42428ee33357b375518
ntvinh1607 <btranmakemewonder@gmail.com>
2020-12-27 02:35:04 +0700

    complete5

--- Triangle.js
+++ Triangle.js
@@ -13,10 +13,10 @@ module.exports = class Triangle {
         if( a<b+c && b<a+c && c<a+b ){ //3 cạnh tạo thành tam giác khi tổng hai cạnh bất kì luôn lớn hơn cạnh còn lại.
             if( a*a==b*b+c*c || b*b==a*a+c*c || c*c== a*a+b*b) //bình phương cạnh huyền bằng tổng bình phương hai cạnh góc vuông
                 return {code:1, message:"Tam giác vuông"};
-            else if(a==b && b==c) // 
-                return {code:2, message:"Tam giác cân"};
+            else if(a==b && b==c) //
+                return {code:2, message:"Tam giác đều"};
             else if(a==b || a==c || b==c)
-                return {code:3, message:"Tam giác đều"};
+                return {code:3, message:"Tam giác cân"};
             else if(a*a > b*b+c*c || b*b > a*a+c*c || c*c > a*a+b*b)    
                 return {code:4, message:"Tam giác tù"};
             else
